4c82744a8cac28eb28d7127bf75d1609497d353b
Manare <manare@mintpass.kr>
2020-09-29 16:11:15 +0900

    코드 최적화 및 버그 수정

--- sleepdog/sleepdog.ino
+++ sleepdog/sleepdog.ino
@@ -1,74 +1,113 @@
+#include <ArduinoJson.h>
+
 #include <SoftwareSerial.h>
+//심박수
+#define USE_ARDUINO_INTERRUPTS true    
+#include <PulseSensorPlayground.h> 
+//G센서
+#include <avr/io.h>
+#include <math.h>
+#include <Wire.h>
+//JSON LIB
 
-float tmp;  // 온도
-int reading;  // tmp 읽어 온 값 저장
-int tmpPin = A0; // 온도센서 핀 번호 지정
-volatile int BPM;                   // 심박수 저장
-volatile int Signal;                // 심장박동센서에서 측정되는 값 저장
-volatile int IBI = 600;             // 심박수 측정 시 사용되는 시간 변수(심장이 몇초마다 뛰는지 측정)
-volatile boolean Pulse = false;     // 유저의 심박수가 측정되면 True, 아무것도 측정되지 않으면 False
-SoftwareSerial bluetooth(2, 3); // RX, TX -> 블루투스 시리얼 연결
-const int xInput = A1; // x 좌표 
-const int yInput = A2; // y 좌표
-const int zInput = A3; // z 좌표 
-
-/// 가속도 센터 변수값 세팅
-int RawMin = 0;
-int RawMax = 1023;
-const int sampleSize = 10;
+#define I2C_ID  0x53
 
 
+//체온 관련
+int temperature;  
+int reading;  
+int lm35Pin = A2;
+
+//심박수 변수
+const int PulseWire = 0;      
+int Threshold = 550;                                                   
+PulseSensorPlayground pulseSensor; 
+ 
+SoftwareSerial BTSerial(2, 3);
+ 
 void setup()  
 {
-    analogReference(INTERNAL);
-    interruptSetup();                 // 센서의 신호를 2ms마다 읽어 심박수로 변환하는 함수
-    Serial.begin(9600);
-    bluetooth.begin(9600);
-}
 
-void loop()  
-{
-  if(bluetooth.available())
-  {
+  analogReference(INTERNAL);
+  Serial.begin(9600);
+  BTSerial.begin(9600);  
 
-  // 체온측정
-    reading = analogRead(tmpPin); // 시리얼로 받아온 값 지정
-    tmp = reading / 9.31; // 온도 값 지정
-    bluetooth.print("TMP : ");
-    bluetooth.println(tmp);
-    // 심박수 측정
-    bluetooth.print("BPM : ");
-    bluetooth.println(BPM);
-    // 가속도 센서
-   int xRaw = ReadAxis(xInput);
-   int yRaw = ReadAxis(yInput);
-   int zRaw = ReadAxis(zInput);
-   
-   float xAccel = map(xRaw, RawMin, RawMax, -3000, 3000)/1000.0;
-   float yAccel = map(yRaw, RawMin, RawMax, -3000, 3000)/1000.0;
-   float zAccel = map(zRaw, RawMin, RawMax, -3000, 3000)/1000.0;
-  
-   Serial.print("X : ");
-   Serial.print(xAccel,0);
-   Serial.println("G");
-   Serial.print("Y : ");
-   Serial.print(yAccel,0);
-   Serial.print("G");
-   Serial.print("Z : ");
-   Serial.print(zAccel,0);
-   Serial.println("G");  
-   delay(1000);
+  Wire.begin();
+
+  // adxl345 initialize
+  // power setting
+  Wire.beginTransmission(I2C_ID);
+  Wire.write(byte(0x2d));
+  Wire.write(byte(0x18));
+  Wire.endTransmission();
+
+  // range setting
+  Wire.beginTransmission(I2C_ID);
+  Wire.write(byte(0x31));
+  Wire.write(byte(0x02));  //0x03
+  Wire.endTransmission();
+
+  //심박수 
+   pulseSensor.analogInput(PulseWire);   
+  pulseSensor.setThreshold(Threshold); 
+
+   if (pulseSensor.begin()) {
+    Serial.println("Sensor OK");  //This prints one time at Arduino power-up,  or on Arduino reset.  
   }
 }
-
-int ReadAxis(int axisPin)
+ 
+void loop()
 {
- long reading = 0;
- analogRead(axisPin);
- delay(1);
- for (int i = 0; i < sampleSize; i++)
- {
- reading += analogRead(axisPin);
- }
- return reading/sampleSize;
+  DynamicJsonDocument doc(1024);
+  //가속도 센서 세팅 
+  char str[30];
+  char data[6];
+  unsigned char idx = 0;
+  short acc_x;
+  short acc_y;
+  short acc_z;
+
+
+
+   reading = analogRead(lm35Pin);
+   temperature = reading / 9.31;
+   
+    // data request
+  Wire.beginTransmission(I2C_ID);
+  Wire.write(byte(0x32));
+  Wire.endTransmission();
+  Wire.requestFrom(0x53, 6);
+
+  idx = 0;
+  while (Wire.available())
+    data[idx++] = Wire.read();
+
+  acc_x = (data[1] << 8) | data[0];
+  acc_y = (data[3] << 8) | data[2];
+  acc_z = (data[5] << 8) | data[4];
+
+ 
+  int myBPM = pulseSensor.getBeatsPerMinute();
+  doc["TEMP"] = temperature;
+  doc["BPM"]   = 0;
+  doc["X"]   = acc_x;
+  doc["Y"]   = acc_y;
+  doc["Z"]   = acc_z;
+if (pulseSensor.sawStartOfBeat()) {                   
+  doc["BPM"]   = myBPM;
+}
+
+ 
+  serializeJson(doc, BTSerial);
+  BTSerial.println();
+
+  serializeJson(doc, Serial);
+  Serial.println();
+  
+
+
+ delay(1000);
+  
+
+   
 }
