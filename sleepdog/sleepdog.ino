3aedcbd5460819924b2f872470f5f08274d81566
Manare <manare@mintpass.kr>
2020-11-05 01:23:03 +0900

    최종버전

--- sleepdog/sleepdog.ino
+++ sleepdog/sleepdog.ino
@@ -27,20 +27,21 @@ PulseSensorPlayground pulseSensor;
 //수면 변수
 int sleep = 0;
 
-//RTC 변수
-ThreeWire myWire(6,5,7); // IO, SCLK, CE
-RtcDS1302<ThreeWire> Rtc(myWire);
 
 
-SoftwareSerial ser(2, 3); // RX/TX 설정, serial 객체생성
+SoftwareSerial esp(2, 3); // RX/TX 설정, serial 객체생성
 
+
+int countTrueCommand;
+int countTimeCommand;
+boolean found = false;
 
 void setup()  
 {
 
   analogReference(INTERNAL);
   Serial.begin(9600);
-  ser.begin(9600);  
+  esp.begin(9600);  
   
 
   Wire.begin();
@@ -70,10 +71,6 @@ void setup()
   }
 
 
-
-
-  Rtc.Begin();
-  RtcDateTime now = Rtc.GetDateTime();
   mlx.begin();
 
   Serial.println("initialization done.");
@@ -87,27 +84,13 @@ void loop()
   DynamicJsonDocument doc(capacity);
 
 
- doc["PRODUCT"]   = 1;
-  //RTC
-  RtcDateTime now = Rtc.GetDateTime();
-  char datestring[12];
-  char ts[6];
+ doc["Product"]   = 1;
 
+ int myBPM = pulseSensor.getBeatsPerMinute();
+              
+ doc["Bpm"]   = myBPM;
 
-    snprintf_P(datestring, 
-            countof(datestring),
-            PSTR("%04u/%02u/%02u"),
-            now.Year(),
-            now.Month(),
-            now.Day());
-
-            snprintf_P(ts, 
-            countof(ts),
-            PSTR("%02u:%02u"),
-            now.Hour(),
-            now.Minute());
-    doc["DATE"]   = datestring;
-    doc["TIME"]   = ts;
+ 
 
 
   //가속도 센서 세팅 
@@ -135,86 +118,91 @@ void loop()
   acc_z = (data[5] << 8) | data[4];
   
 
- 
-  int myBPM = pulseSensor.getBeatsPerMinute();
-  
-  doc["BPM"]   = 0;
-if (pulseSensor.sawStartOfBeat()) {                   
-  doc["BPM"]   = myBPM;
-}
 
   String strTemp = String("");
   strTemp += (double)(mlx.readObjectTempC()); // 주변 온도를 읽습니다.
-  doc["TEMP"] =strTemp;
+  doc["Temp"] =strTemp;
 
    int nsleep = abs(acc_x)+abs(acc_y)+abs(acc_z);
+    
+
 
 
 
   if ( (sleep - 7) <= nsleep && nsleep <= (sleep + 7))
   {
-    doc["CHK"]   = "Sleep";
+    doc["CHK"]   = "0";
     sleep = nsleep;
 
   }
 
   else
   {
-    doc["CHK"]   = "Wake";
+    doc["CHK"]   = "1";
     sleep = nsleep;
   }
   
  
 
 // TCP 연결
-  String cmd = "AT+CIPSTART=\"TCP\",\"";
-  cmd += "sleepdog.mintpass.kr"; // api.thingspeak.com 접속 IP
-  cmd += "\",3000";           // api.thingspeak.com 접속 포트, 80
-  ser.println(cmd);
-   
-  if(ser.find("Error")){
-    Serial.println("AT+CIPSTART error");
-    return;
-  }
-        
+
+String server = "52.231.157.24"; 
+String uri = "/HealthAPI";
+
 
 String output;
   serializeJson(doc, output);
-  
- 
-  int len=output.length();
-  
-
-  
-  String post = "POST /HealthAPI HTTP/1.1\r\n";
-post+= "Host: sleepdog.mintpass.kr\r\nContent-Type: application/json\r\nContent-Length: ";
-delay(1000);
-post+= String(len)+"\r\n\r\n";
-post+=output;
 
 
-
-
- 
-  // Send Data
-  cmd = "AT+CIPSEND=";
-  cmd += String(post.length());
-  ser.println(cmd);
- 
-  if(ser.find(">")){
-    ser.print(post);
-  }
-  else{
-    ser.println("AT+CIPCLOSE");
-    // alert user
-    Serial.println("OK");
-    Serial.println(post);
-  }
-
-
-
-
-
- delay(60000);
+  String postRequest = "POST " + uri  + " HTTP/1.1\r\n" +
+                       "Host: " + server + "\r\n" +
+                       "Accept: *" + "/" + "*\r\n" +
+                       "Content-Length: " + output.length() + "\r\n" +
+                       "Content-Type: application/json\r\n\r\n";
+  postRequest+=output;
+  sendCommandToESP8266("AT+CIPSTART=\"TCP\",\"" + server + "\"," + 3000, 5, "OK");
+  String cipSend = "AT+CIPSEND=" + String(postRequest.length());
+  sendCommandToESP8266(cipSend, 10, ">");
+  sendData(postRequest);
+  sendCommandToESP8266("AT+CIPCLOSE=0", 15, "OK");
+  delay(100);
    
+}
+
+
+void sendCommandToESP8266(String command, int maxTime, char readReplay[]) {
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while (countTimeCommand < (maxTime * 1))
+  {
+    esp.println(command);
+    if (esp.find(readReplay))
+    {
+      found = true;
+      break;
+    }
+    countTimeCommand++;
+  }
+  if (found == true)
+  {
+    Serial.println("Success");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
+  if (found == false)
+  {
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+  found = false;
+}
+
+void sendData(String postRequest) {
+  Serial.println(postRequest);
+  esp.println(postRequest);
+  delay(1500);
+  countTrueCommand++;
 }
